d325a746dc31dd1b451119930fda8adebb770e7a
imFaith <44627279+imFaith@users.noreply.github.com>
2022-10-14 02:00:38 -0400

    challenge 37

---
+++ 8kyu/chellenge-37.js
@@ -0,0 +1,6 @@
+// remove the spaces from the string, then return the resultant string.
+function noSpace(x) {
+    return x.split(' ').join('');
+}
+
+console.log(noSpace('Hello There This Is A Sentence'))
